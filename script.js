a366dcfd37830a290dc7bf999c9983c36b17cfd2
BVBFD <lsevina126@gmail.com>
2021-12-25 00:00:16 +0900

    Code Home Screen Part

---
+++ script.js
@@ -0,0 +1,13 @@
+const menuToggleBtn = document.querySelector(".header .menuIcon");
+const menu = document.querySelector(".menu");
+const menuIcon = document.querySelector(".menuIcon");
+
+const menuToggleBtnClick = () => {
+  menu.classList.toggle("active");
+  menuIcon.classList.toggle("active");
+};
+
+// menuToggleBtn.addEventListener("click", () => {
+//   menu.classList.toggle("active");
+//   menuIcon.classList.toggle("active");
+// });
